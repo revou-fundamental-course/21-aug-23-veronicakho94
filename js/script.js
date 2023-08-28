c317c12b485304ce16e65de8c5003d0c3cc55558
veronicakho94 <kho.veronica@gmail.com>
2023-08-29 03:01:38 +0700

    add index.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+    function replaceName(){
+        let nama = prompt("What's Your Name?","");
+        document.getElementById("nama-user").innerHTML = nama
+    }
+     function load(){
+        replaceName();
+     }
